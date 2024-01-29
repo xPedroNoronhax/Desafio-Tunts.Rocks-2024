316b2d8daefa94d1fcef9b64a174c38cca1b163b
Pedro Noronha <pedro_noronha@outlook.com>
2024-01-29 14:31:02 -0300

    ajusting the formula

--- answear.js
+++ answear.js
@@ -1,7 +1,7 @@
 function calculateAndWriteResults() {
     // Access the active spreadsheet
     var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-    var sheet = spreadsheet.getSheetByName('engenharia_de_software'); // Replace 'engenharia_de_software' with the actual sheet name
+    var sheet = spreadsheet.getSheetByName('engenharia_de_software'); // 
   
     // Get the data range
     var dataRange = sheet.getDataRange();
@@ -12,9 +12,9 @@ function calculateAndWriteResults() {
       var row = values[i];
   
       // Extract relevant data
-      var matricula = row[0];
-      var aluno = row[1];
-      var faltas = row[2];
+      var id = row[0];
+      var student = row[1];
+      var absences  = row[2];
       var p1 = row[3];
       var p2 = row[4];
       var p3 = row[5];
@@ -22,26 +22,26 @@ function calculateAndWriteResults() {
       // Calculate average
       var average = ((p1 + p2 + p3) / 10) / 3;
   
-      // Check for Reproved by Nota
+      // Check for Reproved by grade
       if (average < 5) {
         sheet.getRange(i + 1, 7).setValue('Reprovado por Nota');
         sheet.getRange(i + 1, 8).setValue(0);
       } else if (average < 7) {
-        // Check for Reproved by Falta
-        var totalFaltas = 0.25 * 60; // 25% of total classes
-        if (faltas > totalFaltas) {
+        // Check for Reproved by absences 
+        var totalAbsences  = 0.25 * 60; // 25% of total classes
+        if (absences  > totalAbsences ) {
           sheet.getRange(i + 1, 7).setValue('Reprovado por Falta');
           sheet.getRange(i + 1, 8).setValue(0);
         } else {
-          // Check for Exame Final
-          var naf = Math.ceil(10 - average);
+          // Check for final exam
+          var naf = Math.ceil(10 - average) ;
           sheet.getRange(i + 1, 7).setValue('Exame Final');
           sheet.getRange(i + 1, 8).setValue(naf);
         }
       } else {
-        // Aprovado
-        var totalFaltas = 0.25 * 60; // 25% of total classes
-        if (faltas > totalFaltas) {
+        // Aproved
+        var totalAbsences  = 0.25 * 60; // 25% of total classes
+        if (absences  > totalAbsences ) {
           sheet.getRange(i + 1, 7).setValue('Reprovado por Falta');
           sheet.getRange(i + 1, 8).setValue(0);
         } else {
